8afcd1268e8ef6583c10ce504ee62daa6b9d58c2
jalapagos <jalaj.pun@gmail.com>
2016-05-16 02:10:40 -0700

    Deleted files, need to start again for better flow

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
-var myApp = angular.module('CalorieCounter',['angular-meteor']);
- 
-  myApp.controller('formCtrl',['$scope',function($scope) {
-    $scope.works = function() {
-      alert("Its in the works function");
+var calorieCounterApp = angular.module('CalorieCounter',['angular-meteor']);
+
+calorieCounterApp.controller('formCtrl',['$scope',function($scope) {
+	$scope.works = function() {
+    	alert("Its in the works function");
     }
-  }]);
+}]);
